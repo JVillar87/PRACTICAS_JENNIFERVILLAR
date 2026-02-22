c01bd17bfeb076c8d02386b6ffc02e962a8b138f
JENNIFER <jennifer.villar@estudiant.fjaverianas.com>
2026-02-22 09:56:09 +0100

    Fixing

--- DINO_LLMM_U2_PF1/script.js
+++ DINO_LLMM_U2_PF1/script.js
@@ -6,6 +6,23 @@ var velY = 0;
 var impulso = 900;
 var gravedad = 2500;
 
+var dinoPosX = 42;
+var dinoPosY = floorY;
+
+var floorX = 0;
+var velEscenario = 1280/3;
+var gameVel = 1;
+var score = 0;
+
+var parado = false;
+var jumping = false;
+
+var container;
+var dino; 
+var textScore;
+var floor;
+var gameOver;
+
 if (document.readyState === "complete" || document.readyState === "interactive") {
   setTimeout(Init,1);  
 }else{
@@ -25,6 +42,61 @@ function Loop() {
   requestAnimationFrame(Loop);
 }
 
-function Update() {
+function Start() {
+  gameOver = document.querySelector(".game-over");
+  floor = document.querySelector("#floor");
+  container = document.querySelector("#game-container");
+  textScore = document.querySelector("#score");
+  dino = document.querySelector("#dino");
 
+  document.addEventListener("keydown", HandleKeyDown);
+}
+
+function HandleKeyDown(){
+  if (eval.keyCode == 32) {
+    Saltar();
+  }
+}
+
+function Saltar(){
+  if (dinoPosY === floorY){
+    jumping = true;
+    velY = impulso;
+    dino.classList.remove("dino-running");
+  }
+}
+
+function Update() {
+  MoveFloor();
+  MoveDino();
+
+  velY -= gravedad * deltaTime;
+
+}
+
+function MoveFloor(){
+  floorX += CalcularDesplazamiento();
+  floor.style.left = -(floorX % container.clientWidth) + "px";
+
+}
+
+function MoveDino(){
+  dinoPosY += velY * deltaTime;
+  if(dinoPosY < floorY){
+    TocarSuelo();
+  }
+  dino.style.bottom = dinoPosY+"px";
+}
+
+function TocarSuelo(){
+  dinoPosY = floorY;
+  velY = 0;
+  if (jumping) {
+    dino.classList.add("dino-running");
+  }
+  jumping = false;  
+}
+
+function CalcularDesplazamiento(){
+  return velEscenario * deltaTime * gameVel;
 }
