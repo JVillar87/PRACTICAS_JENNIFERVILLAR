380e61cf22b58d1c44914d9b8ea895072dc3459e
JENNIFER <jennifer.villar@estudiant.fjaverianas.com>
2026-02-26 12:53:16 +0100

    Minor fixes

--- AC25 - FICHEROS/Program.cs
+++ AC25 - FICHEROS/Program.cs
@@ -1,2 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// LIBRERIA
+
+internal class Program
+{
+    static void Main()
+    {
+        /*Simulemos la BBS, los mensajes deben contener usuario, asunto y mensaje separado por ;
+    c3po;para r2d2;A veces simplemente no entiendo el comportamiento humano
+    genera un archivo mensajes.txt con al menos 5 registros (estos serán los de fuera del nodo).
+    Codifica un programa que incluya las siguientes funciones:
+    - Añadir mensaje local (escribe en una lista dinámica)
+    - Lista todos los usuarios
+    - Leer un mensaje local (por usuario)
+    - Leer todos los mensajes locales
+    - Pasar mensajes locales a archivo (mensajes.txt)
+    - Leer todos los mensajes de archivo
+    *recuerda el método .Split de los strings.*/
+
+        StreamWriter mensaje = new StreamWriter("mensajes.txt", true);
+        mensaje.WriteLine("c3po;para r2d2;A veces simplemente no entiendo el comportamiento humano");
+        mensaje.Close();
+
+        string usuario = "";
+        string asunto = "";
+
+
+        StreamWriter ficheros = new StreamWriter(archivos,true);
+        while (producto != "fin")
+        {
+            Console.WriteLine("Escribe el nombre del producto (escribe 'fin' para terminar):");
+            producto = Console.ReadLine();
+            if (producto != "fin")
+            {
+                Console.WriteLine("Escribe el precio del producto:");
+                string precio = Console.ReadLine();
+                ficheros.WriteLine($"{producto}:{precio}");
+            }
+        }
+        ficheros.Close();
+
+
+
+
+
+        string archivo = "libros.txt";
+
+        nuevoLibro(archivo);
+        leerLibro(archivo);
+    }
+
+
+
+    private static void nuevoLibro(string archivo)
+    {
+        Console.WriteLine("título? ");
+        string titulo = Console.ReadLine();
+        Console.WriteLine("autor? ");
+        string autor = Console.ReadLine();
+        Console.WriteLine("isbn? ");
+        string isbn = Console.ReadLine();
+
+        StreamWriter libreria = new StreamWriter(archivo, true);
+
+        libreria.WriteLine("{0};{1};{2}", titulo, autor, isbn);
+
+        libreria.Close();
+    }
+
+    private static void leerLibro(string archivo)
+    {
+        StreamReader libreria = new StreamReader(archivo);
+
+        string linea;
+
+        while ((linea = libreria.ReadLine()) != null)
+        {
+            string[] datosLibro = linea.Split(';');
+
+            Console.WriteLine("Título: {0} ", datosLibro[0]);
+            Console.WriteLine("Autor: {0}", datosLibro[1]);
+            Console.WriteLine("isbn: {0}", datosLibro[2]);
+        }
+        libreria.Close();
+
+    }
+}
--- DINO_LLMM_U2_PF1/script.js
+++ DINO_LLMM_U2_PF1/script.js
@@ -1,30 +1,41 @@
+//VARIABLES GLOBALES
 var time = new Date();
-var deltaTime = 0;
+var deltaTime = 0; //Rastrea milisegundso que tarda en actualizarse el juego
 
-if (document.readyState === "complete" || document.readyState === "interactive") {
-  setTimeout(Init,1);  
-}else{
+if (document.readyState === "complete" || document.readyState === "interactive") 
+//readyState describe el estado de carga del documento.
+//Si es complete o interactive, se llama a Init
+{
+  setTimeout(Init, 1); 
+} else {
   document.addEventListener("DOMContentLoaded", Init);
 }
 
-function Init() {
+//INICIALIZACIÓN DEL JUEGO
+function Init() 
+{
   time = new Date();
   Start();
   Loop();
 }
- 
-function Update() {
-if (parado) return;
 
+//ACTUALIZACIÓN DEL JUEGO
+function Update() {
+  if (parado) return;
+
+  //Llamadas a funciones
   MoveFloor();
   MoveDino();
   AñadirObstaculos();
   MoverObstaculos();
+  DetectarColision();
 
+  //Gravedad
   velY -= gravedad * deltaTime;
 
 }
 
+//BUCLE DEL JUEGO
 function Loop() {
   deltaTime = (new Date() - time) / 1000;
   time = new Date();
@@ -32,6 +43,7 @@ function Loop() {
   requestAnimationFrame(Loop);
 }
 
+//VARIABLES DEL JUEGO
 var floorY = 22;
 var velY = 0;
 var impulso = 950;
@@ -41,7 +53,7 @@ var dinoPosX = 42;
 var dinoPosY = floorY;
 
 var floorX = 0;
-var velEscenario = 1280/3;
+var velEscenario = 1280 / 3;
 var gameVel = 1;
 var score = 0;
 
@@ -56,32 +68,35 @@ var ObstaculoPosY = 16;
 var obstaculos = [];
 
 var container;
-var dino; 
+var dino;
 var textScore;
 var floor;
 var gameOver;
+var restart;
 
 function Start() {
   gameOver = document.querySelector(".game-over");
+  restart = document.querySelector(".restart");
   floor = document.querySelector("#floor");
   container = document.querySelector("#game-container");
   textScore = document.querySelector("#score");
   dino = document.querySelector("#dino");
   cactus = document.querySelector("#cactus");
 
+
   document.addEventListener("keydown", HandleKeyDown);
 }
 
 // ACCIÓN SALTO CON BARRA ESPACIADORA (32)
-function HandleKeyDown(ev){
-  if (ev.keyCode == 32) { 
+function HandleKeyDown(ev) {
+  if (ev.keyCode == 32) {
     Jump();
   }
 }
 
-//ACCIÓN DE SALTO
-function Jump(){
-  if (dinoPosY === floorY){
+//SALTO
+function Jump() {
+  if (dinoPosY === floorY) {
     jumping = true;
     velY = impulso;
     dino.classList.remove("dino-running");
@@ -89,105 +104,124 @@ function Jump(){
 }
 
 //MOVIMIENTO A LA IZQ DEL SUELO
-function MoveFloor(){
+function MoveFloor() {
   floorX += CalcularDesplazamiento();
   floor.style.left = -(floorX % container.clientWidth) + "px";
 }
 
-function MoveDino(){
-  dinoPosY += velY * deltaTime;
-  if(dinoPosY < floorY){
+//MOVIMIENTO DEL DINOSAURIO
+function MoveDino() {
+  dinoPosY += velY * deltaTime; //Calcula el desplazamiento del dino (velocidad * tiempo)
+  if (dinoPosY < floorY) {
     TocarSuelo();
   }
-  dino.style.bottom = dinoPosY+"px";
+  dino.style.bottom = dinoPosY + "px"; //Actualiza la posición del dino en el eje Y
 }
 
-function TocarSuelo(){
+//EL DINOSAURIO TOCA EL SUELO
+function TocarSuelo() {
   dinoPosY = floorY;
   velY = 0;
   if (jumping) {
     dino.classList.add("dino-running");
   }
-  jumping = false;  
+  jumping = false;
 }
 
-function CalcularDesplazamiento(){
-  return velEscenario * deltaTime * gameVel;
+//CALCULO DE DESPLAZAMIENTO
+function CalcularDesplazamiento() {
+  return velEscenario * deltaTime * gameVel; 
+  //Calcula el desplazamiento del escenario (velocidad * tiempo * velocidad del juego)
 }
 
+//APARICIÓN DE OBSTACULOS
 function AñadirObstaculos() {
-  tiempoHastaObstaculo -= deltaTime;
+  tiempoHastaObstaculo -= deltaTime; //Tiempo entre obstaculos
   if (tiempoHastaObstaculo <= 0) {
     CrearObstaculo();
   }
 }
 
-//APARICIÓN OBSTACULOS
 function CrearObstaculo() {
   var obstaculo = document.createElement("div");
   container.appendChild(obstaculo);
   obstaculo.classList.add("cactus");
   obstaculo.posX = container.clientWidth;
-  obstaculo.style.left = container.clientWidth+"px";
+  obstaculo.style.left = container.clientWidth + "px"; //Posición inicial del obstaculo
 
-  obstaculos.push(obstaculo);
-  tiempoHastaObstaculo = tiempoObstaculoMin + Math.random() * 
-  (tiempoObstaculoMax-tiempoObstaculoMin) / gameVel;
+  obstaculos.push(obstaculo); //Añade el nuevo obstaculo al array de obstaculos
+  tiempoHastaObstaculo = tiempoObstaculoMin + Math.random() *
+    (tiempoObstaculoMax - tiempoObstaculoMin) / gameVel; 
 }
 
 //MOVIMIENTO DEL CACTUS
 function MoverObstaculos() {
-    for (var i = obstaculos.length - 1; i >= 0; i--) {
-        if(obstaculos[i].posX < -obstaculos[i].clientWidth) {
-            obstaculos[i].parentNode.removeChild(obstaculos[i]);
-            obstaculos.splice(i, 1);
-            GetPoints();
-        }else{
-            console.log(obstaculos[i].posX);
-            obstaculos[i].posX -= CalcularDesplazamiento();
-            obstaculos[i].style.left = obstaculos[i].posX+"px";
-        }
-    } 
+  for (var i = obstaculos.length - 1; i >= 0; i--) {
+    if (obstaculos[i].posX < -obstaculos[i].clientWidth) {
+      obstaculos[i].parentNode.removeChild(obstaculos[i]);
+      obstaculos.splice(i, 1);
+      GetPoints();
+    } else {
+      // console.log(obstaculos[i].posX);
+      obstaculos[i].posX -= CalcularDesplazamiento();
+      obstaculos[i].style.left = obstaculos[i].posX + "px";
+    }
+  }
 }
 
 //PUNTOS
-function GetPoints(){
+function GetPoints() {
   score++;
   textScore.innerHTML = score
 }
 
+//ELIMINADO
 function GameOver() {
-    Crash();
-    gameOver.style.display = "block";
+  Crash();
+  gameOver.style.display = "block";
 }
+
+//RESETEAR JUEGO
+// function Restart() {
+//   document.addEventListener("keydown", function(ev) {
+//     if (collision && ev.keyCode == 82) { //Rrrr
+//       location.reload();
+//     }
+//   });
+// } 
+
 
 //COLISIÓN CONTRA CACTUS
 function Crash() {
-    dino.classList.remove("dino-corriendo");
-    dino.classList.add("dino-estrellado");
-    parado = true;
+  dino.classList.remove("dino-running");
+  dino.classList.add("dino-crashed");
+  parado = true;
 }
 
 function DetectarColision() {
-    for (var i = 0; i < obstaculos.length; i++) {
-        if(obstaculos[i].posX > dinoPosX + dino.clientWidth) {
-            break; 
-        }else{
-            if(IsCollision(dino, obstaculos[i], 10, 30, 15, 20)) {
-                GameOver();
-            }
-        }
+  for (var i = 0; i < obstaculos.length; i++) {
+    if (obstaculos[i].posX > dinoPosX + dino.clientWidth) {
+      //EVADE COLISIÓN
+      break; //al estar en orden, no puede chocar con más
+    } else {
+      if (IsCollision(dino, obstaculos[i], 10, 30, 15, 20)) {
+        GameOver();
+      }
     }
+  }
 }
 
 function IsCollision(a, b, paddingTop, paddingRight, paddingBottom, paddingLeft) {
-    var aRect = a.getBoundingClientRect();
-    var bRect = b.getBoundingClientRect();
+  var aRect = a.getBoundingClientRect(); //Devuelve tamaño objeto y su posición relativa al viewport
+  var bRect = b.getBoundingClientRect();
 
-    return !(
-        ((aRect.top + aRect.height - paddingBottom) < (bRect.top)) ||
-        (aRect.top + paddingTop > (bRect.top + bRect.height)) ||
-        ((aRect.left + aRect.width - paddingRight) < bRect.left) ||
-        (aRect.left + paddingLeft > (bRect.left + bRect.width))
-    );
+  let collision = (
+    ((aRect.top + aRect.height - paddingBottom) < (bRect.top)) ||
+    (aRect.top + paddingTop > (bRect.top + bRect.height)) ||
+    ((aRect.left + aRect.width - paddingRight) < bRect.left) ||
+    (aRect.left + paddingLeft > (bRect.left + bRect.width))
+  ) //Calcula si hay colisión entre a y b.
+  // Tiene en cuenta el padding para ajustar la zona de colisión
+
+  return !collision;
 }
